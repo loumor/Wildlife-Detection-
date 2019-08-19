437e56ea713f6f54551a4295e5c22597263931ae
Brandon Janson <bjanson28@gmail.com>
2019-08-19 21:22:18 +1000

    add dolphin images and labels

--- scripts/vid2img.py
+++ scripts/vid2img.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import pickle
 import os, cv2
 
-src = 'sample_dolphins_1'
-video_inp = 'data/' + src + '.mp4'
-img_out = 'data/img/' + src + '.jpg'
+src = 'Drones Over Dolphin Stampede_QUT Classroom_copyright 2014 David Anderson'
+video_inp = '../data/' + src + '.MP4'
+img_out = '../data/img/' + src + '.jpg'
 
 video_reader = cv2.VideoCapture(video_inp)
 nb_frames = int(video_reader.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -19,7 +19,11 @@ print('Number of frames to process: {}'.format(nb_frames))
 
 for i in range(nb_frames):
     ret, image = video_reader.read()
+    # cv2.imshow(str(i), image)
+    if i % 25 != 0:
+        continue
 
-    cv2.imwrite('data/img/' + src + "_" + str(i) + ".jpg", image)
+    cv2.imwrite('../data/img/' + src + "_" + str(i) + ".jpg", image)
+    print('Processed frame: ', i)
 
 video_reader.release()
